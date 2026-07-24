784da2a3c030dfb966852af9706397ab015c7c4f
Raunak <newerxd1@gmail.com>
2026-07-24 23:06:10 +0530

    util fix

--- src/lib/zod-utils.ts
+++ src/lib/zod-utils.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
-export function parseOrThrow<T>(schema: z.ZodSchema<T>, data: unknown): T {
+export function parseOrThrow<S extends z.ZodTypeAny>(schema: S, data: unknown): z.output<S> {
   try {
-    return schema.parse(data);
+    return schema.parse(data) as z.output<S>;
   } catch (err) {
     if (err instanceof z.ZodError) {
       const msgs = err.issues.map((i) => `${i.path.join(".")}: ${i.message}`).join("; ");
